5f18cf72855601d8da44cf2d5a7b63f2d736e84b
Matthew Gould <matt@everettgould.com>
2016-10-09 21:37:26 -0400

    reviewing material

--- ex2/ex1.c
+++ ex2/ex1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /* This is a comment. */
 int main(int argc, char *argv[])
 {
--- ex3/ex3.c
+++ ex3/ex3.c
@@ -5,8 +5,8 @@ int main(int argc, char *argv[])
     int age = 100;
     int height = 72;
 
-    printf("I am %d years old.\n", argv);
-    printf("I am %d inches tall.\n", height);
+    printf("I am %d argc and %s argv.\n", argc, argv[0]);
+    printf("I am %d inches tall and %d years old.\n", height, age);
 
     return 0;
 }
